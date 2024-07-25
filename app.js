35263e83147b6ad989e9d96653be47937d122a83
Tati Tablada <tatitablada14@gmail.com>
2024-07-25 15:57:32 -0300

    Se hicieron peticiones POST, PATCH y DELETE, ademas se agregaron errores de ruta.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const {connectToMongoDB, disconnectFromMongoDB} = require('./src/database.js')
+const { connectToMongoDB, disconnectFromMongoDB } = require('./src/database.js')
 const app = express()
 const port = process.env.PORT ?? 3000
 const morgan = require('morgan')
-const { ObjectId } = require('mongodb')
 const Prenda = require('./schema/product.js');
+
 
 
 app.use(express.json())
@@ -13,20 +13,21 @@ app.get('/', (req, res) => {
   res.json('Bienvenidos a la API de prendas')
 })
 
-app.use('/prendas', connectToMongoDB, async (req,res,next) => {
-    res.on('finish', async () => {
-    await disconnectFromMongoDB(req,res,next)
-    })
-    next()
-})
+app.use('/prendas', connectToMongoDB);
+app.use('/prendas', async (req, res, next) => {
+  res.on('finish', async () => {
+    await disconnectFromMongoDB(req, res, next);
+  });
+  next();
+});
 
 
 // Se muestran todas las prendas y se filtran por nombre
 app.get('/prendas', async (req, res) => {
   try {
     const { nombre } = req.query;
-    const filterName = nombre ? { nombre: { $regex: `.*${nombre}.*`, $options: 'i' } } : {};
-    const prendas = await req.db.find(filterName).sort({ codigo: 1 }).toArray();
+    const filterName = !nombre ? {} : { nombre: { $regex: `.*${nombre}.*`, $options: 'i' } };
+    const prendas = await Prenda.find(filterName).sort({ codigo: 1 });
     res.json(prendas);
   } catch (error) {
     console.error('Error al obtener las prendas:', error);
@@ -38,8 +39,7 @@ app.get('/prendas', async (req, res) => {
 app.get('/prendas/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const objectId = new ObjectId(id);
-    const prenda = await req.db.findOne({ _id: objectId });
+    const prenda = await Prenda.findById(id);
     if (prenda) {
       return res.json(prenda);
     }
@@ -49,18 +49,73 @@ app.get('/prendas/:id', async (req, res) => {
   }
 });
 
+// Se crea una nueva prenda
 app.post('/prendas', async (req, res) => {
-  const nuevaPrenda = new Prenda(req.body);
-   try {
-    await nuevaPrenda.save();
-    res.status(201).json(nuevaPrenda);
-   } catch (error) {
+  try {
+    const { codigo, nombre, precio, categoria } = req.body;
+    if (!codigo || !nombre || !precio || !categoria) {
+      return res.status(400).json({ message: 'Todos los campos son obligatorios' });
+    }
+
+    const nuevaPrenda = new Prenda({
+      codigo,
+      nombre,
+      precio,
+      categoria,
+    });
+
+    const prendaGuardada = await nuevaPrenda.save();
+    res.status(201).json(prendaGuardada);
+  } catch (error) {
     console.error('Error al crear la prenda:', error);
-    res.status(500).json({ error: 'Error al crear la prenda' });
+    res.status(500).json({ message: 'Error al crear la prenda', error: error.message });
   }
 });
 
+// Se modifica el precio de la prenda
 
+app.patch('/prendas/:id', async (req, res) => {
+  try {
+    const { precio } = req.body;
+    if (!precio) {
+      return res.status(400).json({ message: 'El precio es requerido' })
+    }
+
+    const prendaActualizada = await Prenda.findByIdAndUpdate(req.params.id, { precio }, { new: true });
+    if (!prendaActualizada) {
+      return res.status(404).json({ message: 'Prenda no encontrada' });
+    }
+    res.json(prendaActualizada);
+  } catch (error) {
+    console.error('Error al actualizar el precio de la prenda', error);
+    res.status(500).json({ message: 'Error al actualizar el precio de la prenda' });
+  }
+});
+
+// Se elimina una prenda por id
+
+app.delete('/prendas/:id', async (req, res) => {
+  try {
+    const prendaEliminada = await Prenda.findByIdAndDelete(req.params.id);
+    if (!prendaEliminada) {
+      return res.status(404).json({ message: 'Prenda no encontrada' });
+    }
+    res.json({ message: 'Prenda eliminada correctamente', prenda: prendaEliminada });
+  } catch (error) {
+    console.error('Error al eliminar la prenda', error);
+    res.status(500).json({ message: 'Error al eliminar la prenda' });
+  }
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Ruta no encontrada' });
+});
+
+
+app.use((err, req, res, next) => {
+  console.error('Error interno del servidor:', err);
+  res.status(500).json({ message: 'Error interno del servidor' });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
--- schema/product.js
+++ schema/product.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose')
 
 const prendaSchema = new mongoose.Schema({
-    codigo: Number,
-    nombre: String,
-    precio: Number,
-    categoria: String,
-
+    codigo: { type: Number, required: true },
+    nombre: { type: String, required: true },
+    precio: { type: Number, required: true },
+    categoria: { type: String, required: true },
 });
 
-const Prenda =  mongoose.model('Prenda', prendaSchema)
+const Prenda = mongoose.model('Prenda', prendaSchema)
 module.exports = Prenda;
